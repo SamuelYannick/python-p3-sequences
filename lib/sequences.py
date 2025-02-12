0d787763ab50c80222b4bb63de0dabff1ebc54f8
SamuelOchieng <o.samuelyannick@gmail.com>
2025-02-12 12:27:30 +0300

    Fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
+
+# Steps: 
+# Define 2 inital variables 0 and 1
+# Use for loop n times while adding the previous value with the current value
+# Print the values(the sum of the last 2 numbers) as the current value
 
 def print_fibonacci(length):
-    pass
+    value_1, value_2 = 0, 1
+    print(value_1, end = ", ")
+    print(value_2, end = ", ")
+    for i in range(length):
+        new_value = value_1 + value_2
+        print(new_value, end = ", ")
+        value_1 = value_2
+        value_2 = new_value
+
+print_fibonacci(10)
+
